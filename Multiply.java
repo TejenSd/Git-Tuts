e451692dfa2e857fb6dff2d7a856f41f2fb227dd
Tejendra <tejenderdanu46@gmail.com>
2022-04-24 21:53:43 +0530

    added multiply.java

---
+++ Multiply.java
@@ -0,0 +1,9 @@
+public class Multiply {
+    public static void main(String args[])
+    {
+        int n1 = 225, n2 = 115, mul;
+        mul = n1 * n2;
+        System.out.println("The mul of numbers is: " + mul);
+        
+    }
+}
