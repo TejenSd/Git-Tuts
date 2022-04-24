42da6a1c3ea931637f13603ee6f50734bb3c8275
Tejendra <tejenderdanu46@gmail.com>
2022-04-24 22:17:04 +0530

    commit for githubpush

--- Multiply.java
+++ Multiply.java
@@ -4,6 +4,7 @@ public class Multiply {
         int n1 = 225, n2 = 115, mul;
         mul = n1 * n2;
         System.out.println("The mul of numbers is: " + mul);
-        
+     
+        // push fpr github
     }
 }
